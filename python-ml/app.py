dff3d397680c9ce6465b50ed144365f14217ac79
Vedant Shelar <vedantshelar871@gmail.com>
2026-03-29 12:06:26 +0530

    app.py updated

--- python-ml/app.py
+++ python-ml/app.py
@@ -295,4 +295,5 @@ def check_neural_core():
     })
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
